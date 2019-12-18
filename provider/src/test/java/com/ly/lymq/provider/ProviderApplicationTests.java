b9b0d3afd35086bcb1d0ccf1666a2b2958248e25
liaoyi <liaoyi@qding.me>
2019-12-18 17:05:56 +0800

    新建项目

---
+++ provider/src/test/java/com/ly/lymq/provider/ProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ly.lymq.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
